1488aba064867de3a62ab01bfca7c7a9d184561e
gkansala13 <kansalag@oregonstate.edu>
2022-06-07 11:24:27 -0700

    Replaced loop with pipeline

--- cherry/lib/main.rb
+++ cherry/lib/main.rb
@@ -15,14 +15,26 @@ def total_and_smallest
   [total, smallest]
 end
 
+# def total_and_smallest
+#   data = [23, 15, 8, 16, 4, 42]
+#   smallest = data.first
+#   data.each do |d|
+#     if d < smallest
+#       smallest = d
+#     end
+#     total = 0
+#     data.each do |d|
+#       total += d
+#     end
+#   end
+#   [total, smallest]
+# end
+
 
 # Apply the "replace loop with pipeline" refactoring.
 
 def repeat
   data = ["This", "is", "the", "data", "to", "use"]
-  result = ""
-  (0..5).each do |i|
-    result += data[i]
-  end
-  result
+  data.join("") 
 end
+
